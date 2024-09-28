6d1dc161b379b5538eea6dfa6a2bd0ef09e2bae5
ParthParmar2711 <pparmar271122@gmail.com>
2024-09-28 13:11:06 +0530

    video project

---
+++ video-vite/src/App.jsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import VideoContainer from "./VideoContainer"
+
+const App = () => {
+  return (
+    <>
+      <VideoContainer/>
+    </>
+  )
+}
+
+export default App
