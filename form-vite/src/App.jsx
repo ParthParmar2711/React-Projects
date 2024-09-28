1d25acfb8d347654a21148f53c50421ca6b3fab7
ParthParmar2711 <pparmar271122@gmail.com>
2024-09-28 13:15:45 +0530

    form-project

---
+++ form-vite/src/App.jsx
@@ -0,0 +1,31 @@
+// // eslint-disable-next-line no-unused-vars
+// import React from 'react'
+// import FormValidate from "./FormValidate"
+// const App = () => {
+//   return (
+//     <>
+//       <FormValidate/>
+//     </>
+//   )
+// }
+
+// export default App
+
+
+
+//!---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import FormRegex from "./FormRegex"
+const App = () => {
+  return (
+    <>
+     <FormRegex/>
+    </>
+  )
+}
+
+export default App
